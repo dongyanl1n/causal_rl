66888d2a39d8828bd9c4156ba7945d98929ab60e
lindongy <lindongy@mila.quebec>
2024-05-02 14:45:29 -0400

    added callback for logging quiz-type; added num_eval_episodes arg and mechanism to save eval traj

--- causal_overhypotheses/driver.py
+++ causal_overhypotheses/driver.py
@@ -3,11 +3,46 @@ from stable_baselines.ppo2.ppo2 import PPO2
 from envs.causal_env_v0 import CausalEnv_v0, ABconj, ACconj, BCconj, Adisj, Bdisj, Cdisj
 from stable_baselines.common.vec_env.dummy_vec_env import DummyVecEnv
 from stable_baselines.common.misc_util import set_global_seeds
-from stable_baselines.common.callbacks import EvalCallback, StopTrainingOnRewardThreshold
+from stable_baselines.common.callbacks import EvalCallback, StopTrainingOnRewardThreshold, CallbackList, BaseCallback
 import argparse
 from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy, MlpLnLstmPolicy
 from functools import partialmethod
 import os
+import numpy as np
+import tensorflow as tf
+
+class CustomTensorboardCallback(BaseCallback):
+    """
+    Custom callback for logging additional rewards to TensorBoard. Use for quiz-type where there's quiz reward and type reward.
+    """
+    def __init__(self, verbose=0):
+        super(CustomTensorboardCallback, self).__init__(verbose)
+        self.is_tb_set = False
+
+    def _on_step(self) -> bool:
+        # Accessing each sub-environment in a vectorized environment
+        quiz_rewards = np.mean([env.get_quiz_reward() for env in self.model.env.envs])
+        type_rewards = np.mean([env.get_type_reward() for env in self.model.env.envs])
+
+        # Set up the TensorBoard summary logging only once
+        if not self.is_tb_set:
+            with self.model.graph.as_default():
+                self.quiz_reward_ph = tf.placeholder(tf.float32, shape=None, name="quiz_reward")
+                self.type_reward_ph = tf.placeholder(tf.float32, shape=None, name="type_reward")
+                self.quiz_reward_summary = tf.summary.scalar('quiz_reward', self.quiz_reward_ph)
+                self.type_reward_summary = tf.summary.scalar('type_reward', self.type_reward_ph)
+                self.summary_op = tf.summary.merge([self.quiz_reward_summary, self.type_reward_summary])
+            self.is_tb_set = True
+
+        # Log the values using the placeholders and summaries
+        feed_dict = {
+            self.quiz_reward_ph: quiz_rewards,
+            self.type_reward_ph: type_rewards
+        }
+        summary = self.model.sess.run(self.summary_op, feed_dict)
+        self.locals['writer'].add_summary(summary, self.num_timesteps)
+
+        return True
 
 
 
@@ -136,7 +171,11 @@ def main(args):
 
     # Stop training when the model reaches the reward threshold
     # callback_on_best = StopTrainingOnRewardThreshold(reward_threshold=3, verbose=1)
-    eval_callback = EvalCallback(eval_env, callback_on_new_best=None, eval_freq=5000, verbose=1, n_eval_episodes=100)
+    eval_callback = EvalCallback(eval_env, callback_on_new_best=None, eval_freq=5000, verbose=1, n_eval_episodes=args.num_eval_episodes)  # Using evaluate_policy_save with threshold=2.5
+    
+    if args.reward_structure == 'quiz-type':
+        log_callback = CustomTensorboardCallback()  # Log quiz and type rewards to TensorBoard during quiz-type env
+        eval_callback = CallbackList([eval_callback, log_callback])
 
     if args.policy == 'mlp':
         policy = MlpPolicy
@@ -165,9 +204,12 @@ def main(args):
         total_timesteps=int(args.num_steps),
         callback=eval_callback
     )
-    # os.makedirs("/network/scratch/l/lindongy/causal_overhypotheses/model_output/{}".format(save_name), exist_ok=True)
-    # model.save("/network/scratch/l/lindongy/causal_overhypotheses/model_output/{}/model".format(save_name))
+    os.makedirs("/network/scratch/l/lindongy/causal_overhypotheses/model_output/{}".format(save_name), exist_ok=True)
+    model.save("/network/scratch/l/lindongy/causal_overhypotheses/model_output/{}/model".format(save_name))
 
+    slurm_tmpdir = os.environ.get('SLURM_TMPDIR', '/tmp')  # where eval trajectories are saved
+    os.makedirs("/network/scratch/l/lindongy/causal_overhypotheses/model_output/{}/eval_traj/".format(save_name), exist_ok=True)
+    os.system(f"mv {slurm_tmpdir}/* /network/scratch/l/lindongy/causal_overhypotheses/model_output/{save_name}/eval_traj/")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train a model')
@@ -178,6 +220,8 @@ if __name__ == "__main__":
     parser.add_argument('--quiz_disabled_steps', type=int, default=-1, help='Number of quiz disabled steps (-1 for no forced exploration)')
     parser.add_argument('--holdout_strategy', type=str, default='none', help='Holdout strategy')
     parser.add_argument('--reward_structure', type=str, default='quiz', help='Reward structure')
+    parser.add_argument('--num_eval_episodes', type=int, default=200, help='Number of evaluation episodes')
     args = parser.parse_args()
-    print(args)
+    argsdict = args.__dict__
+    print(argsdict)
     main(args)
