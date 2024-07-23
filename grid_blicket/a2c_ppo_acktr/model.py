6804765470a1fcc9f54593e9e47e5bf8c79c93c2
lindongy <lindongy@mila.quebec>
2024-07-22 21:14:01 -0400

    added gru

--- grid_blicket/a2c_ppo_acktr/model.py
+++ grid_blicket/a2c_ppo_acktr/model.py
@@ -33,7 +33,6 @@ class Policy(nn.Module):
 
         self.dist = Categorical(self.base.output_size, action_space.n)
 
-
     @property
     def is_recurrent(self):
         return self.base.is_recurrent
@@ -89,7 +88,6 @@ class ResidualBlock(nn.Module):
         out = self.conv1(out)
         out = nn.ReLU()(out)
         out = self.conv2(out)
-        #print(wefw)
         return out + x
 
 class ImpalaBlock(nn.Module):
@@ -120,14 +118,17 @@ class ImpalaModel(nn.Module):
 
         self.fc = nn.Linear(in_features=n_flatten, out_features=hidden_size)
 
+        self._recurrent = recurrent
+        self._hidden_size = hidden_size
+
+        if self._recurrent:
+            self.gru = nn.GRU(hidden_size, hidden_size)
+
         self.output_dim = hidden_size
         self.apply(xavier_uniform_init)
         init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.constant_(x, 0), nn.init.calculate_gain('relu'))
         self.critic_linear = init_(nn.Linear(hidden_size, 1))
         self.train()
-
-        self._recurrent = recurrent
-        self._hidden_size = hidden_size
 
     def _get_conv_output(self, x):
         x = self.block1(x / 255.0)
@@ -148,6 +149,64 @@ class ImpalaModel(nn.Module):
     def output_size(self):
         return self._hidden_size
 
+    def _forward_gru(self, x, hxs, masks):
+        if x.size(0) == hxs.size(0):
+            x, hxs = self.gru(x.unsqueeze(0), (hxs * masks).unsqueeze(0))
+            x = x.squeeze(0)
+            hxs = hxs.squeeze(0)
+        else:
+            # x is a (T, N, -1) tensor that has been flatten to (T * N, -1)
+            N = hxs.size(0)
+            T = int(x.size(0) / N)
+
+            # unflatten
+            x = x.view(T, N, x.size(1))
+
+            # Same deal with masks
+            masks = masks.view(T, N)
+
+            # Let's figure out which steps in the sequence have a zero for any agent
+            # We will always assume t=0 has a zero in it as that makes the logic cleaner
+            has_zeros = ((masks[1:] == 0.0) \
+                            .any(dim=-1)
+                            .nonzero()
+                            .squeeze()
+                            .cpu())
+
+            # +1 to correct the masks[1:]
+            if has_zeros.dim() == 0:
+                # Deal with scalar
+                has_zeros = [has_zeros.item() + 1]
+            else:
+                has_zeros = (has_zeros + 1).numpy().tolist()
+
+            # add t=0 and t=T to the list
+            has_zeros = [0] + has_zeros + [T]
+
+            hxs = hxs.unsqueeze(0)
+            outputs = []
+            for i in range(len(has_zeros) - 1):
+                # We can now process steps that don't have any zeros in masks together!
+                # This is much faster
+                start_idx = has_zeros[i]
+                end_idx = has_zeros[i + 1]
+
+                rnn_scores, hxs = self.gru(
+                    x[start_idx:end_idx],
+                    hxs * masks[start_idx].view(1, -1, 1)
+                )
+
+                outputs.append(rnn_scores)
+
+            # assert len(outputs) == T
+            # x is a (T, N, -1) tensor
+            x = torch.cat(outputs, dim=0)
+            # flatten
+            x = x.view(T * N, -1)
+            hxs = hxs.squeeze(0)
+
+        return x, hxs
+
     def forward(self, x, rnn_hxs, masks):
         x = self.block1(x / 255.0)
         x = self.block2(x)
@@ -156,5 +215,8 @@ class ImpalaModel(nn.Module):
         x = Flatten()(x)
         x = self.fc(x)
         x = nn.ReLU()(x)
+
+        if self.is_recurrent:
+            x, rnn_hxs = self._forward_gru(x, rnn_hxs, masks)
 
         return self.critic_linear(x), x, rnn_hxs
