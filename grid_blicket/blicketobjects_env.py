b26109b9dc91ad8cfdff81b4f34328eb713259fa
lindongy <lindongy@mila.quebec>
2024-08-30 21:11:52 -0400

    wip

--- grid_blicket/blicketobjects_env.py
+++ grid_blicket/blicketobjects_env.py
@@ -3,6 +3,7 @@ import numpy as np
 import gymnasium as gym
 from gymnasium import spaces
 import miniworld
+from miniworld.entity import Entity
 from miniworld.entity import Box, Key, Ball
 from miniworld.miniworld import MiniWorldEnv
 from miniworld.params import DEFAULT_PARAMS
@@ -74,6 +75,7 @@ class BlicketObjectsEnv(MiniWorldEnv):
 
         # Allow movement actions, pickup, and drop
         self.action_space = spaces.Discrete(self.actions.drop + 1)
+        self.episode_log = []
 
     def _gen_world(self):
         # Create a new room
@@ -97,43 +99,107 @@ class BlicketObjectsEnv(MiniWorldEnv):
         # Place other objects randomly
         self.box = self.place_entity(Box(color=self.np_random.choice(['red', 'blue', 'purple', 'yellow']), size=0.9))
         self.ball = self.place_entity(Ball(color=self.np_random.choice(['red', 'blue', 'purple', 'yellow']), size=0.9))
-        self.key = self.place_entity(Key(color=self.np_random.choice(['red', 'blue', 'purple', 'yellow'])))
+        # self.key = self.place_entity(Key(color=self.np_random.choice(['red', 'blue', 'purple', 'yellow'])))
 
         # Place the agent
         self.place_agent()
 
         self.objects_at_goal = set()
+        self.episode_log = []
 
     def step(self, action):
-        obs, reward, terminated, truncated, info = super().step(action)
+        """
+        Perform one action and update the simulation
+        """
 
-        # Small punishment for each step
+        self.step_count += 1
         reward = -0.01
+        termination = False
+        truncation = False
+        info = {}
+        step_log = ""
 
-        # Handle pickup action
-        if action == self.actions.pickup:
-            if self.agent.carrying is None:
-                for obj in [self.box, self.ball, self.key]:
-                    if self.near(obj):
-                        self.agent.carrying = obj
-                        reward += 0.1
-                        break
+        rand = self.np_random if self.domain_rand else None
+        fwd_step = self.params.sample(rand, "forward_step")
+        fwd_drift = self.params.sample(rand, "forward_drift")
+        turn_step = self.params.sample(rand, "turn_step")
 
-        # Handle drop action
+        if action == self.actions.move_forward:
+            self.move_agent(fwd_step, fwd_drift)
+
+        elif action == self.actions.move_back:
+            self.move_agent(-fwd_step, fwd_drift)
+
+        elif action == self.actions.turn_left:
+            self.turn_agent(turn_step)
+
+        elif action == self.actions.turn_right:
+            self.turn_agent(-turn_step)
+
+        # Pick up an object
+        elif action == self.actions.pickup:
+            # Position at which we will test for an intersection
+            test_pos = self.agent.pos + self.agent.dir_vec * 1.5 * self.agent.radius
+            ent = self.intersect(self.agent, test_pos, 1.2 * self.agent.radius)
+            if not self.agent.carrying:
+                if isinstance(ent, Entity):
+                    if not ent.is_static:
+                        self.agent.carrying = ent
+                        ##################
+                        step_log += f"Step {self.step_count}: "
+                        step_log += f"Agent picked up {ent.__class__.__name__}"
+
+        # Drop an object being carried
         elif action == self.actions.drop:
-            if self.agent.carrying is not None:
-                # Check if near the goal
-                if self.near(self.goal_box):
-                    self.objects_at_goal.add(self.agent.carrying)
-                    reward += 1.0
+            if self.agent.carrying:
+                ent = self.agent.carrying
+                self.agent.carrying.pos[1] = 0
                 self.agent.carrying = None
+                #####################
+                if self.near(self.goal_box):
+                    self.objects_at_goal.add(ent)
+                    step_log += f"Step {self.step_count}: "
+                    step_log += f"Agent dropped {ent.__class__.__name__} at goal"
+                    print(f"Agent dropped {ent.__class__.__name__} at goal!")
+                else:
+                    step_log += f"Step {self.step_count}: "
+                    step_log += f"Agent dropped {ent.__class__.__name__}"
+
+        # If we are carrying an object, update its position as we move
+        if self.agent.carrying:
+            ent_pos = self._get_carry_pos(self.agent.pos, self.agent.carrying)
+            self.agent.carrying.pos = ent_pos
+            self.agent.carrying.dir = self.agent.dir
+
+        # Generate the current camera image
+        obs = self.render_obs()
 
         # Check if all objects are at the goal
-        if len(self.objects_at_goal) == 3:
-            reward += 10.0
-            terminated = True
+        if len(self.objects_at_goal) == 2:
+            step_log += f"Step {self.step_count}: "
+            step_log += "All objects at goal!"
+            print("All objects at goal!")
+            reward = 10.0
+            termination = True
+            truncation = False
+        
+        # If the maximum time step count is reached
+        elif self.step_count >= self.max_episode_steps:
+            termination = False
+            truncation = True
 
-        return obs, reward, terminated, truncated, info
+        
+        if step_log != "":
+            self.episode_log.append(step_log)
+        info['episode_log'] = self.episode_log
+
+        return obs, reward, termination, truncation, info
+    
+
+    def reset(self, **kwargs):
+        self.episode_log = []  # Reset log at the start of each episode
+        return super().reset(**kwargs)
+
 
 
 def main():
@@ -173,8 +239,63 @@ def main():
 
     print("Episode saved as 'blicket_objects_episode.gif'")
 
+def debug_blicket_objects_env(n_episodes=10, max_steps=500):
+    # Create the environment
+    env = BlicketObjectsEnv(size=10, max_episode_steps=max_steps)
+    
+    # Set random seeds for reproducibility
+    random.seed(42)
+    np.random.seed(42)
 
+    # set up arrays for logging
+    rewards_arr = np.zeros((n_episodes, max_steps+1))
+    actions_arr = np.zeros((n_episodes, max_steps+1))
+    terminated_arr = np.zeros((n_episodes, max_steps+1))
+    truncated_arr = np.zeros((n_episodes, max_steps+1))
+    total_rewards_arr = np.zeros(n_episodes)
+
+
+    for i_episode in range(n_episodes):
+        print(f"\nEpisode {i_episode + 1}")
+        print("===============")
+    
+        # Reset the environment
+        obs = env.reset()
+        
+        done = False
+        step = 0
+        total_reward = 0
+        
+        while not done:
+            # Choose a random action
+            action = env.action_space.sample()
+            
+            # Take a step in the environment
+            obs, reward, terminated, truncated, info = env.step(action)
+            
+            # Update total reward and step count
+            total_reward += reward
+            step += 1
+            
+            # Log step information
+            rewards_arr[i_episode, step] = reward
+            actions_arr[i_episode, step] = action
+            terminated_arr[i_episode, step] = int(terminated)
+            truncated_arr[i_episode, step] = int(truncated)
+            
+            # Check if the episode is done
+            done = terminated or truncated
+        total_rewards_arr[i_episode] = total_reward
+        print("\nEpisode finished!")
+        print(f"Total steps: {step}")
+        print(f"Total reward: {total_reward:.2f}")
+        if total_reward > -0.001*max_steps:
+            print(f"  Log: {env.episode_log}")
+        
+    # Close the environment
+    env.close()
 
 if __name__ == '__main__':
-    main()
+    # main()
+    debug_blicket_objects_env(n_episodes=10, max_steps=500)
 
